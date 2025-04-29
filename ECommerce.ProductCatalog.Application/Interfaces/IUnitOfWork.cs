3bc714fcfa86c841db86e00a49d6925560db894d
lemuayala <lemuayala@gmail.com>
2025-04-29 00:18:43 -0300

    Interfaz para UoW

---
+++ ECommerce.ProductCatalog.Application/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+namespace ECommerce.ProductCatalog.Application.Interfaces;
+
+public interface IUnitOfWork : IAsyncDisposable
+{
+    ICategoryRepository Categories { get; }
+    IProductRepository Products { get; }
+
+    // Método para confirmar todos los cambios realizados en esta "unidad"
+    Task<int> CompleteAsync(CancellationToken cancellationToken = default);
+}
